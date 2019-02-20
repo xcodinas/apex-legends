6631f6d21d7310ef04c2885b4fdba42e99514dcd
Xavier Codinas <xavier19966@gmail.com>
2019-02-20 11:37:39 +0100

    Increase version to add the new files to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='apex-legends',
-    version='0.1.0',
+    version='0.1.1',
     description='Python wrapper for https://apex.tracker.gg/ api.',
     long_description=long_description,
     long_description_content_type='text/markdown',
